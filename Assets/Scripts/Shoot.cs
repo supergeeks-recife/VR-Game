968f3dfc03e5a7acc6fb058542fa0a7bc5989abb
supergeeks-recife <recife.supergeeks@gmail.com>
2022-10-25 15:37:48 -0300

    Script Shoot

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Atirar();
+        }
+    }
+
+    public void Atirar()
+    {
+        Debug.Log("Pá!");
+    }
+}
