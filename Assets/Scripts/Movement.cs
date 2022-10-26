ada1e91fa67c16963614d483147ab8b7902dcc84
supergeeks-recife <recife.supergeeks@gmail.com>
2022-10-26 11:53:00 -0300

    add new folders and scripts

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
