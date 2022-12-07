d572d2dc1780feff01755f3b18ba8d8ea1e89660
supergeeks-recife <recife.supergeeks@gmail.com>
2022-12-07 17:56:03 -0300

    SpawnObject Script

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject objeto;
+    public Transform[] spawnPointList;
+
+    void Start()
+    {
+        int index = Random.Range(0, spawnPointList.Length);
+        Instantiate(objeto, spawnPointList[index]);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
